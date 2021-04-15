75a11e61c256ccea5a0e46b987f13e2b1db953b8
Mattias Kindborg <73967018+mattias-kindborg-at-work@users.noreply.github.com>
2021-04-15 23:23:37 +0200

    docs: images to aws s3 (#1)

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# TODO
+# Axis integrations into Amazon Web Services (AWS)
+
+## Getting started with the repo
+
+This repository aims to build upon [AxisCommunications/acap3-examples](https://github.com/AxisCommunications/acap3-examples/), at a time when you find yourself content with the functionality of the camera but wish to integrate it with AWS.
+
+The repository contains a set of examples, each tailored towards a specific problem. All examples have a README file in its directory which shows overview, example directory structure and step-by-step instructions on how to deploy the application on AWS and how to configure the camera to meet the expectations of AWS.
+
+## Example applications
+
+Below is the list of examples available in the repository.
+
+- [images-to-aws-s3](./images-to-aws-s3/)
+  - This example covers sending images from a camera to AWS S3
+
+## License
+
+[Apache 2.0](LICENSE)
---
+++ images-to-aws-s3/src/index.js
@@ -0,0 +1,81 @@
+/**
+ * Copyright (C) 2021 Axis Communications AB, Lund, Sweden
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const AWS = require("aws-sdk");
+const env = require("./env");
+const response = require("./response");
+const secrets = require("./secrets");
+
+const s3 = new AWS.S3();
+
+const post = async (event) => {
+  const apiAccessToken = await secrets.getApiAccessToken();
+
+  if (event?.queryStringParameters?.accessToken !== apiAccessToken) {
+    return response.Forbidden;
+  }
+
+  const contentType = event.headers["content-type"];
+  if (!contentType || !contentType.startsWith("image/")) {
+    return response.UnsupportedMediaType;
+  }
+
+  const contentDisposition = event.headers["content-disposition"];
+  if (!contentDisposition) {
+    return response.BadRequest;
+  }
+
+  const filename = contentDisposition.match(
+    /^attachment;\s*filename=\"(?<filename>.*)\"$/
+  )?.groups?.filename;
+  if (!filename) {
+    return response.BadRequest;
+  }
+
+  const encoding = event.isBase64Encoded ? "base64" : "binary";
+  const body = Buffer.from(event.body, encoding);
+
+  await s3
+    .upload({
+      Bucket: env.bucketName,
+      Key: filename,
+      Body: body,
+    })
+    .promise();
+
+  return response.OK;
+};
+
+const get = async () => {
+  return response.OK;
+};
+
+const handler = async (event) => {
+  switch (event.requestContext.http.method) {
+    case "GET":
+      return get();
+
+    case "POST":
+      return post(event);
+
+    default:
+      return response.MethodNotAllowed;
+  }
+};
+
+module.exports = {
+  handler,
+};
